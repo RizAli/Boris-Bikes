ed9727d6bdf493b38a7620f88cc9140419893116
Rizwan Ali <rizcube@gmail.com>
2015-04-30 16:48:03 +0100

    DRYing out public_accesses_bike_spec

--- spec/features/public_accesses_bike_spec.rb
+++ spec/features/public_accesses_bike_spec.rb
@@ -2,20 +2,19 @@ require 'docking_station'
 
 feature 'member of public accesses bike' do
 
+  let(:docking_station) { DockingStation.new }
+
   scenario 'docking station releases a working bike' do
-    docking_station = DockingStation.new
     docking_station.dock Bike.new
     bike = docking_station.release_bike
     expect(bike).not_to be_working
   end
 
   scenario 'docking station does not release a bike when there are none available' do
-    docking_station = DockingStation.new
     expect { docking_station.release_bike }.to raise_error "No bikes available"
   end
 
   scenario 'docking station does not release broken bikes' do
-    docking_station = DockingStation.new
     bike = Bike.new
     bike.report_broken
     docking_station.dock bike
